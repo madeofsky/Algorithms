8b0d9d150fc8e7a90cd0813fa7835784631bb24a
Lucas Fernandes <blslucasfj1@hotmail.com>
2023-07-18 12:13:11 -0300

    - Implemented FizzBuzz algorithm in Kotlin and Java

--- app/src/main/java/com/lukita/algorithms/fizzbuzz/FizzBuzzJava.java
+++ app/src/main/java/com/lukita/algorithms/fizzbuzz/FizzBuzzJava.java
@@ -1,4 +1,45 @@
 package com.lukita.algorithms.fizzbuzz;
 
+//O(n) Algorithm
 public class FizzBuzzJava {
+
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final String FIZZBUZZ = "Fizz Buzz";
+
+    public void printFizzBuzz(Integer numbersListSize) {
+        String[] fizzBuzzList = new String[numbersListSize + 1];
+
+        for (int i = 0; i <= numbersListSize; i++) {
+            if (isFizz(i)) {
+                fizzBuzzList[i] = FIZZ;
+            } else if (isBuzz(i)) {
+                fizzBuzzList[i] = BUZZ;
+            } else if (isFizzBuzz(i)) {
+                fizzBuzzList[i] = FIZZBUZZ;
+            } else {
+                fizzBuzzList[i] = Integer.toString(i);
+            }
+        }
+
+        printFizzBuzzList(fizzBuzzList);
+    }
+
+    private boolean isFizz(int number) {
+        return number % 3 == 0;
+    }
+
+    private boolean isBuzz(int number) {
+        return number % 5 == 0;
+    }
+
+    private boolean isFizzBuzz(int number) {
+        return isFizz(number) && isBuzz(number);
+    }
+
+    private void printFizzBuzzList(String[] fizzBuzzList) {
+        for (int i = 0; i <= fizzBuzzList.length; i++) {
+            System.out.println(fizzBuzzList[i]);
+        }
+    }
 }
--- app/src/main/java/com/lukita/algorithms/fizzbuzz/FizzBuzzKotlin.kt
+++ app/src/main/java/com/lukita/algorithms/fizzbuzz/FizzBuzzKotlin.kt
@@ -1,4 +1,38 @@
 package com.lukita.algorithms.fizzbuzz
 
+//O(n) Algorithm
 class FizzBuzzKotlin {
+
+    fun printFizzBuzz(numbersListSize: Int) {
+        val fizzBuzzList = mutableListOf<String>()
+
+        for (number in 0..numbersListSize) {
+            when {
+                isFizzBuzz(number) -> fizzBuzzList.add(FIZZBUZZ)
+                isFizz(number) -> fizzBuzzList.add(FIZZ)
+                isBuzz(number) -> fizzBuzzList.add(BUZZ)
+                else -> fizzBuzzList.add(number.toString())
+            }
+        }
+
+        printFizzBuzzList(fizzBuzzList)
+    }
+
+    private fun isFizz(number: Int): Boolean = number % 3 == 0
+
+    private fun isBuzz(number: Int): Boolean = number % 5 == 0
+
+    private fun isFizzBuzz(number: Int): Boolean = isFizz(number) && isBuzz(number)
+
+    private fun printFizzBuzzList(fizzBuzzList: List<String>) {
+        fizzBuzzList.forEach {
+            println(it)
+        }
+    }
+
+    companion object {
+        const val FIZZ = "Fizz"
+        const val BUZZ = "Buzz"
+        const val FIZZBUZZ = "Fizz Buzz"
+    }
 }
